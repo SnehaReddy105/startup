152f702ef755d3ad4cb4f0307cf55fb1dc93bef1
SnehaReddy105 <119911379+SnehaReddy105@users.noreply.github.com>
2023-03-01 09:16:43 +0530

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -3,15 +3,20 @@ import streamlit as st
 import numpy as np
 import pandas as pd
 import re
-import nltk
 import string
+import nltk
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+from nltk.stem.porter import PorterStemmer
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn.tree import DecisionTreeRegressor
 from sklearn.ensemble import RandomForestClassifier
 
-
+nltk.download('punkt')
+nltk.download('stopwords')
+sw=nltk.corpus.stopwords.words("english")
 
 rad=st.sidebar.radio("Navigation",["Home","Sentiment Analysis","Sarcasm Detection"])
 
